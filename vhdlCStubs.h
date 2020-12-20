c7df3508cc00d7bcbdcfc03e09535be8768ecdc3
mitulty <mitultyagi45@gmail.com>
2020-12-20 23:27:57 +0530

    Final Codes

---
+++ vhdlCStubs.h
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <Pipes.h>
+#include <SocketLib.h>
+uint32_t DOTP(uint8_t R);
+uint32_t dotP_0(uint8_t R);
+uint32_t dotP_1(uint8_t R);
+void global_storage_initializer_();
+void multiplyMatrixVector();
+void mvp_daemon();
+void readMatrix();
+void readVector();
